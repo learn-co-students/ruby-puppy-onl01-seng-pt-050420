1d5dd4f706c7c2d476ef526f5eaeb70e93621061
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-20 17:46:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    self.save
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.clear_all
+    return @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+  end
+  
+  def save
+    @@all.push(self)
+  end
+  
+end
