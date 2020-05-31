2e4674b5824f3e364d5f84f5ef25e29de91f26f8
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-31 01:35:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
-# Add your code here
+
+class Dog
+  attr_accessor :name
+  
+  @@all=[]
+
+  def initialize(name)
+    @name = name
+    self.save
+  end
+  
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each {|dog| puts "#{dog.name}"}
+  end
+
+def self.print_all
+  @@all.collect {|dog| puts "#{dog.name}"}
+end
+def save
+  @@all << self
+  
+end
+
+end
